ff5642f2fd6336cbd592d83b4644bea2a9ba88af
Justyna Konior <jkonior01@gmail.com>
2026-08-19 12:45:29 +0200

    Recognizing the center of motion added

--- frame_differencing.py
+++ frame_differencing.py
@@ -19,6 +19,38 @@ def get_mask(frame1, frame2, kernel):
 
     return mask
 
+def get_center_of_motion(mask, display_frame, min_area=500, max_area = 75000):
+    # mask, type of detection (ignore insides of objects), method of compression
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    best_contour = None
+    best_area = 0
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if min_area < area < max_area:
+            x, y, w, h = cv2.boundingRect(contour)
+            ratio = float(w/h)
+
+            # additional filter:
+            if 0.1 < ratio < 5.0:
+                if area > best_area:
+                    best_area = area
+                    best_contour = contour
+
+    if best_contour is not None:
+        x, y, w, h = cv2.boundingRect(best_contour)
+        center_x = int(x + w / 2)
+        center_y = int(y + h / 2)
+
+        cv2.rectangle(display_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.circle(display_frame, (center_x, center_y), 5, (0, 0, 255), -1)
+
+        return center_x, center_y
+
+    return None, None
+
+
 def main():
     # CAP_DSHOW - DirectShow, it overwrites the standard way of Windows opening
     # camera feed with MSMF, which is incompatible with Iriun
@@ -42,12 +74,22 @@ def main():
             right1 = cv2.cvtColor(right_frame_1, cv2.COLOR_BGR2GRAY)
             right2 = cv2.cvtColor(right_frame_2, cv2.COLOR_BGR2GRAY)
 
-            kernel = np.array((16,16), dtype=np.uint8)
+            kernel = np.ones((16, 16), np.uint8)
 
             mask_left = get_mask(left2, left1, kernel)
             mask_right = get_mask(right2, right1, kernel)
-            
 
+            left_x, left_y = get_center_of_motion(mask_left, left_frame_2)
+            right_x, right_y = get_center_of_motion(mask_right, right_frame_2)
+
+    
+            cv2.imshow("Left detection", left_frame_2)
+            cv2.imshow("Right detection", right_frame_2)
+
+            # should be changed to yield or other method
+            if left_x is not None or right_x is not None:
+                print(f"L: ({left_x}, {left_y})") #  | R: ({right_x}, {right_y})
+            
             if cv2.waitKey(1) & 0xFF == KEY_ESC:
                 break
     finally: 
